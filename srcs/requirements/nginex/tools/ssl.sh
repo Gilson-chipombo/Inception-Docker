7346b4a90439ba0b8b36f5400cded94b9faf4b75
Gilson-chipombo <fernandochipombo@gmail.com>
2026-01-13 16:09:16 +0100

    Nginex configurations

---
+++ srcs/requirements/nginex/tools/ssl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Criar certificado SSL se nao existir
+
+if [! -f /etc/nginx/ssl/nginx.crt ]; then
+    openssl req -x509 -nodes -days 365 \
+    -newkey rsa:2048 \
+    -keyout /etc/nginx/ssl/nginx.key \
+    -out /etc/nginx/ssl/nginx.crt \
+    -subj "/C=AO/ST=Luanda/L=Luanda/O=42/OU=Inception/CN=localhost"
+fi
+# Iniciar NGINX em foreground
+exec nginx -g "daemon off;"
